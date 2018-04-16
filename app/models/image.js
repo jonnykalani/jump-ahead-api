aff5b4939803f49ab915ad2ccf72d9fa1bda91bb
jonnykalani <jonathankhicks@gmail.com>
2018-04-16 11:45:57 -0400

    Adds virtuals to images
    Added virtuals true to image model

--- app/models/image.js
+++ app/models/image.js
@@ -19,6 +19,7 @@ const imageSchema = new mongoose.Schema({
 }, {
   timestamps: true,
   toJSON: {
+    virtuals: true,
     transform: function (doc, ret, options) {
       const userId = (options.user && options.user._id) || false
       ret.editable = userId && userId.equals(doc._owner)
